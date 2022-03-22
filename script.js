687fdc096f530a4cd1dd6161c44dffb5e2a7c9ca
Du-devBR <eduardo.ananiasdev@outlook.com>
2022-03-22 19:11:08 -0300

    Feito validação ao adionar os cards

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let getInputUrl = document.querySelector('#inputUrl')
 let getInputBtn = document.querySelector('#buttonSubmit')
 let mainContent = document.querySelector('#addContent')
 let arrayHeroes = []
+let btnViewMore = document.querySelector('#openModal')
+let modalHero = document.querySelector('#modalOn')
+
 
 
 getInputBtn.addEventListener('click', function(event){
@@ -13,14 +16,40 @@ getInputBtn.addEventListener('click', function(event){
 
     imagem: getInputUrl.value,
     nome: getInputName.value,
-    descricao: getInputDescription.value,
+    descricao: getInputDescription.value
 
   }
+
+  // Valdiação dos campos input
+  let msgAlert = ''
+  if(heroObject.imagem === ''){
+
+    msgAlert += 'Informe uma url de imagem valida \n'
+  }
+
+  if(heroObject.nome === ''){
+
+    msgAlert += 'informe o nome do super heroi \n'
+  }
+
+  if(heroObject.descricao === ''){
+
+    msgAlert += 'informe a descrição do super heroi \n'
+  }
+
+  if(msgAlert != ''){
+
+    alert(msgAlert)
+    return false
+
+  }
+
 
   arrayHeroes.unshift(heroObject) //utilizado UNSHIFT para adionar ao começo do array
   console.log(arrayHeroes)
 
 
+  modalHero.innerHTML = ''
   mainContent.innerHTML = '' // metodo para quando adionar os campos no html, não respostar o array anterior
   for(let post of arrayHeroes) {
 
@@ -28,19 +57,38 @@ getInputBtn.addEventListener('click', function(event){
 
     <div class="cards">
         <img src="${post.imagem}">
+        <div class = "name_description">
         <h3>${post.nome}</h3>
         <p>${post.descricao}</p>
+        <button class="viewMore" id="lito" onclick="openModal()">Ver Galeria</button>
+        </div>
     </div>
     `
+
+    modalHero.innerHTML += `
+
+    <header class="nameHero">
+      <h3>${post.nome}</h3>
+    </header>
+    <div class="container_modal">
+        <img src="${post.imagem}">
+        <p>${post.descricao}"</p>
+        <button class="viewMore" onclick="closeModal()">Fechar</button>
+        </div>
+    `
   }
-
-
 
 })
 
 
+// Função para abrir modal
+function openModal() {
+  modalHero.classList.add('open')
+}
 
+// Função para fechar o modal
 
-// console.log(getInputDescription)
-// console.log(getInputUrl)
-// console.log(getInputBtn)
+function closeModal() {
+
+  modalHero.classList.remove('open')
+}
